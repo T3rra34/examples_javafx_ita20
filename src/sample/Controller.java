165f0a1605d9b35bbbd5196f11ffb070df8c1b82
Henri Kazbekov <kazbekov.henri@khk.ee>
2021-05-26 01:23:21 +0300

    Tegin failid väiksemaks

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,10 @@
 package sample;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-
 public class Controller {
-
     @FXML
     private  Button btn;
-
     @FXML
     private void click(ActionEvent event) {
         btn.setText("You've clicked");
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,28 +1,21 @@
 package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 public class Main extends Application{
     public static void main(String[] args) {
         Application.launch(args);
     }
-
     @Override
     public void start(Stage stage) throws Exception{
-
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Scene scene = new Scene(root);
-
         stage.setScene(scene);
-
         stage.setTitle("Hello JavaFX");
         stage.setWidth(250);
         stage.setHeight(200);
-
         stage.show();
     }
 }
